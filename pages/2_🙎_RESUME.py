3d81e82a14d373530328001e671c6621b0c8eb75
SUDHANSHU <gusainsudhanshu43@gmail.com>
2023-10-20 15:22:42 +0530

    full

--- pages/2_🙎_RESUME.py
+++ pages/2_🙎_RESUME.py
@@ -10,7 +10,7 @@ st.set_page_config(
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "my_resume97.pdf"
+resume_file = current_dir / "assets" / "Resume-Sudhanshu-Gusain (3).pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
